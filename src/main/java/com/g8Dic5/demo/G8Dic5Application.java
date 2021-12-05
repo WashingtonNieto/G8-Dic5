2876b0aa1fffee381d1b9744ecb3e517d49ee654
Washington Nieto <washington_nieto@hotmail.com>
2021-12-05 07:47:17 -0500

    First Commit

---
+++ src/main/java/com/g8Dic5/demo/G8Dic5Application.java
@@ -0,0 +1,13 @@
+package com.g8Dic5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G8Dic5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G8Dic5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/g8Dic5/demo/G8Dic5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g8Dic5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G8Dic5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
